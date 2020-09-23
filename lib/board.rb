f666ab5d46eb019d1921fafff047c6c7ca8ed715
Damilola Oduronbi <doduronbi@gmail.com>
2020-09-23 15:20:07 +0100

    Get player positions and update board

--- lib/board.rb
+++ lib/board.rb
@@ -1,66 +1,35 @@
+# rubocop:disable Metrics/CyclomaticComplexity,Metrics/PerceivedComplexity
 # This handles the updating and display of board
 class Board
-  # logic goes here
-attr_accessor :board
+  attr_accessor :board
+
   def initialize
     @board = [
-      ['-','-','-'],
-      ['-','-','-'],
-      ['-','-','-']
+      [' ', ' ', ' '],
+      [' ', ' ', ' '],
+      [' ', ' ', ' ']
     ]
   end
+
   def display
-    a = @board[0][0]
-    b = @board[0][1]
-    c = @board[0][2]
-    d = @board[1][0]
-    e = @board[1][1]
-    f = @board[1][2]
-    g = @board[2][0]
-    h = @board[2][1]
-    i = @board[2][2]
     "
-    #{a} | #{b} | #{c}
-    #{d} | #{e} | #{f}
-    #{g} | #{h} | #{i}
+    #{@board[0][0]} | #{@board[0][1]} | #{@board[0][2]}
+    #{@board[1][0]} | #{@board[1][1]} | #{@board[1][2]}
+    #{@board[2][0]} | #{@board[2][1]} | #{@board[2][2]}
     "
   end
 
-  def update(&user_input)
-    input =  user_input.call
-    puts "Your input is #{input} !"
-    puts "Your token is #{$player_token} !"
-    while input
-      
-    if input.to_i >= 1 && input.to_i <=9
-    if input == 1
-      @board[0][0] = $player_token
-    elsif input == 2
-      @board[0][0] = $player_token
-    elsif input == 3
-      @board[0][0] = $player_token
-    elsif input == 4
-      @board[0][0] = $player_token
-    elsif input == 5
-      @board[0][0] = $player_token
-    elsif input == 6
-      @board[0][0] = $player_token
-    elsif input == 7
-      @board[0][0] = $player_token
-    elsif input == 8
-      @board[0][0] = $player_token
-    elsif input == 9
-      @board[0][0] = $player_token
-    end
-  #  unless exist 
-  #   puts "Position taken ! Please choose another one :"
-  #   input = user_input.call
-  #   end
-    input = false
-    else 
-      puts "Please enter a valid position"
-      input = user_input.call
-    end
-    end
+  def update_position(val, token)
+    @board[0][0] = token if val == 1
+    @board[0][1] = token if val == 2
+    @board[0][2] = token if val == 3
+    @board[1][0] = token if val == 4
+    @board[1][1] = token if val == 5
+    @board[1][2] = token if val == 6
+    @board[2][0] = token if val == 7
+    @board[2][1] = token if val == 8
+    @board[2][2] = token if val == 9
+    display
   end
 end
+# rubocop:enable Metrics/CyclomaticComplexity,Metrics/PerceivedComplexity
