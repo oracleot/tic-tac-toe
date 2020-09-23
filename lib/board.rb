03bbecd03327a4c11b2422ea14bc16f8dfbcb902
Damilola Oduronbi <doduronbi@gmail.com>
2020-09-23 09:33:10 +0100

    Setup project structure

---
+++ lib/board.rb
@@ -0,0 +1,5 @@
+# This handles the updating and display of board
+class Board
+  # logic goes here
+  puts 'Board loaded'
+end
---
+++ lib/players.rb
@@ -0,0 +1,5 @@
+# This handles getting inputs from the players
+class Players
+  # logic goes here...
+  puts 'Players loaded'
+end
